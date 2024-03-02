79eb8a2a59d28db814f312d2c4832fae1c2a97de
Rachelz85 <darthrachelz@gmail.com>
2024-03-02 14:30:21 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+function runGame() {
+
+}
+
+function chackAnswer() {
+
+}
+
+function calculateCorecctAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
+
